14a8996c1f8bedc8cffacc7cd191b0fd9d4d408c
ajeftani <78095454+jeftani@users.noreply.github.com>
2023-07-16 00:49:07 +0100

    Update echo.c

--- last1.3/builtins/echo.c
+++ last1.3/builtins/echo.c
@@ -1,80 +1,19 @@
 #include "../minishell.h"
 
-void putstr_trim(char *cmd,int end,int begining)
+void putstr_trim(char *cmd)
 {
-    int i;
-    int j;
-    j = 0;
-    i = 0;
-    if(end && begining)
-        {
-            int len = ft_strlen(&cmd[i]);
-            //while(cmd[i])
-            //{
-            //&& cmd[i] != '\"' && cmd[i] != '\''
-            write(1,"h",1);
-            while(cmd[i] && i < (len - 2) )
-            {
-               i++;
-                write(1,&cmd[i],1);
-            }
-            //i++;
-            //}
-        }
-    if(end)
-        {
-         int len = ft_strlen(&cmd[i]);
-        while(cmd[i])
-            {
-            while(cmd[i] && i < (len - 1) && cmd[i] != '\"' && cmd[i] != '\'')
-            {
-                write(1,&cmd[i],1);
-                i++;
-            }
-                i++;
-            }
-        }
-      if(begining)
-        {
-            while(cmd[i])
-            {
-            while(cmd[i] && cmd[i] != '\"' && cmd[i] != '\'')
-            {
-                //i++;
-                write(1,&cmd[i],1);
-                i++;
-            }
-            i++;
-            }
-        }
-    else
-        {
-            while(cmd[i])
-            {
-                
-                write(1,&cmd[i],1);
-                i++;
-            }
-        }
- }
+    int i = 0;
 
-int quote_there(char c)
-{
-    if(c == '\'' || c == '\"')
-        return(1);
-    else
-        return(0);
-}
+            while(cmd[i])
+            {
+                write(1,&cmd[i],1);
+                i++;
+            }
+ }
 
 void output_echo(char **cmd,int i)
 {
-    int begining;
-    int end;
-    begining = quote_there(cmd[i][0]);
-        int len = ft_strlen(cmd[i]);
-        //I removed backslash zero . end = quote_there(cmd[i][len - 1]);
-    end = quote_there(cmd[i][len]);
-        putstr_trim(cmd[i],end,begining);
+        putstr_trim(cmd[i]);
         if(cmd[i + 1])
             write(1," ",1);
 }
